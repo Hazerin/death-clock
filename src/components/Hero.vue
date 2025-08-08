2c7e890f6ea49bb91047e1ad4c7d71fc6bdc854e
Hazerin <alby91v@gmail.com>
2025-08-08 11:08:06 +0200

    checkpoint

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -11,6 +11,32 @@
             <!--Oltre che col punto, si può accedere al dato con la corrispondente stringa usata come chiave-->
             {{ name }}, you have {{ data["weeks"] }} weeks left. Make them count😄
         </h3>
+        <div class="btns-container">
+            <button>
+                Not {{ name }}?
+            </button>
+            <button class="link-button">
+                Copy link
+            </button>
+            <button class="link-button">
+                Reset data
+            </button>
+        </div>
+        <div class="progress-bar">
+            <div>
+                <div>
+                    <i class="fa-solid fa-baby"></i>
+                    <h6 class="bar-label">Birth</h6>
+                </div>
+                <h6>
+                    {{ 45 }}%
+                </h6>
+            </div>
+            <div>
+                <h6 class="bar-label">Death</h6>
+                <i class="fa-solid fa-skull"></i>
+            </div>
+        </div>
     </section>
 </template>
 
--- src/components/layouts/Layout.vue
+++ src/components/layouts/Layout.vue
@@ -11,6 +11,8 @@
         <small>Created by</small>
         <a href="https://github.com/Hazerin" target="_blank">
             <img src="/hazipfp.jpg" alt="pfp"/>
+            <p>Hazerin</p>
+            <i class="fa-brands fa-github"></i>
         </a>
     </footer>
 </template>
